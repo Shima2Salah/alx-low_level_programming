49733937fb6529068e30059e4ba2b529b3fba627
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-11 16:29:40 -0700

    a program that prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - the entery point
+ *
+ * Description: program prints possible combinations of four digits
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n1, n2, n3, n4;
+
+	for (n1 = 48; n1 <= 57; n1++)
+	{
+		for (n2 = 48; n2 <= 57; n2++)
+		{
+			for (n3 = 48; n3 <= 57; n3++)
+			{
+				for (n4 = 48; n4 <= 57; n4++)
+				{
+					if (((n1 + n2) < (n3 + n4)) && (n1 <= n3))
+					{
+					putchar(n1);
+					putchar(n2);
+					putchar(' ');
+					putchar(n3);
+					putchar(n4);
+					if ((n1 != 57) || (n2 != 56) || (n3 != 57) || (n4 != 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
