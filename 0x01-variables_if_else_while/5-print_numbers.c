adf30326d102f339d05dd078ef1c609f27100978
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-11 14:12:05 -0700

    a program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the entery point
+ *
+ * Description: program to print all numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
+
