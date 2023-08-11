7b7b1b7982454ca76511a572add0d4eca67f2003
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-11 09:47:16 -0700

    a program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Description: program to write all lowercase letters
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
+
