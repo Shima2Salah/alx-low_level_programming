0d0a023652991768e53d8fc33ccaf6b758854e9f
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-08-16 03:59:28 +0300

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_to_98 - funtion to print to 98 in ascending r desc order
+ *
+ * @n: integer 
+ */
+
+void print_to_98(int n)
+{
+	int num;
+
+	if (n <= 98)
+	{
+		for (num = n; num < 98; num++)
+			printf("%d, ", num);
+	}
+	else
+	{
+		for (num = n; num > 98; num--)
+			printf("%d, ", num);
+	}
+	printf("98\n");
+}
