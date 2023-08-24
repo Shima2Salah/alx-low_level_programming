bf1cc607c77df15c5095b99a001c1e695b596a09
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-24 11:45:04 -0700

    a function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - a function that turns upper
+ * @a: input pointer
+ *
+ * Return: pointer
+ */
+char *leet(char *a)
+{
+	char *b;
+	int i, j;
+	char c[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char d[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	for (i = 1; a[i] != '\0'; i++)
+	{
+		for (j = 0; d[j] != '\0'; j++)
+		{
+			if (a[i] == d[j])
+			{
+				a[i] = c[j];
+			}
+		}
+	}
+	b = a;
+	return (b);
+}
+
