2c06ecfa8c7631f918ff475a032c8b6dfec4dba8
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-24 13:39:54 -0700

    a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string
+ * @a: input pointer
+ *
+ * Return: pointer
+ */
+char *rot13(char *a)
+{
+	char *b;
+	int i, j;
+	char c[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; d[j] != '\0'; j++)
+		{
+			if (a[i] == d[j])
+			{
+				a[i] = c[j];
+			}
+		}
+	}
+	b = a;
+	return (b);
+}
