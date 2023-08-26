55f86307897a43d8781e34845f93099d83b17b77
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-08-26 17:19:48 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,7 +22,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[size_r - 1] = '\0';
 	i = len1 - 1;
 	j = len2 - 1;
-	while (i >= 0 || j >= 0 || carry)
+	while (i >= 0 || j >= 0 || carry != 0)
 	{
 		digit1 = i >= 0 ? n1[i] - '0' : 0;
 		digit2 = j >= 0 ? n2[j] - '0' : 0;
