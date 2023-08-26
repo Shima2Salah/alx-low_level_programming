7f1447afbbffac9d3e280c0d32f77a6d08a810da
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-08-26 20:33:25 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -37,7 +37,7 @@ void print_buffer(char *b, int size)
 			if (i % 2)
 				putchar(' ');
 		}
-		for (i = 0; i <= 10 - (size + 1) % 10; i++)
+		for (i = 0; i <= 10 - ((size + 1) % 10); i++)
 			printf("  ");
 		for (i = 0; i < 5 - ((size) % 10 / 2); i++)
 			putchar(' ');
