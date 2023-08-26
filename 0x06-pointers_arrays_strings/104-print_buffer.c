9cf1be260b826404acbee10d06928c7d440ec24a
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-08-26 18:58:30 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,6 +18,21 @@ for (i = 0; i < size; i += 10)
 printf("%08x: ", i);
 for (j = i; j < i + 10; j++)
 {
+if (j < size)
+{
+printf("%02x", b[j]);
+}
+else
+{
+printf("  ");
+}
+if (j % 2)
+{
+printf(" ");
+}
+} 
+for (j = i; j < i + 10; j++)
+{
 if (j >= size)
 {
 printf(" ");
