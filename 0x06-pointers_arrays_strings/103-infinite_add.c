ea4b7e829a025eb05f3ceba863a52b1e1cb013d1
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-08-26 14:52:43 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,70 +1,82 @@
 /**
- * infinite_add - funtion to add to large numbers
- * @n1: address of first number
- * @n2: address of second number
- * @r: address to store the result
- * @size_r: length of the @r array
- */
-
+* infinite_add - Adds two numbers.
+* @n1: First number.
+* @n2: Second number.
+* r: Buffer to store the result.
+* @size_r: Size of buffer r.
+* Return: Result of sum. On error return 0.
+*
+* init - Initializes pointers needed for main to work.
+* @n1: n1.
+* @n2: n2.
+* @l1: l1.
+* @l2: l2.
+* @size_r: size_r.
+* @min: min.
+* @imin: imin.
+* @max: max.
+* @imax: imax.
+* @c1: c1.
+* @c2: c2.
+*/
+int init(char **n1, char **n2, int *l1, int *l2, int size_r, char **min,
+char **imin, char **max, char **imax, char **c1, char **c2)
+{
+	while (*(*n1)++)
+		(*l1)++;
+	while (*(*n2)++)
+		(*l2)++;
+	*n1 -= 2;
+	*n2 -= 2;
+	if (*l1 >= size_r || *l2 >= size_r)
+		return (0);
+	if (*l1 > *l2)
+	{
+		*min = *n2;
+		*imin = *c2;
+		*max = *n1;
+		*imax = *c1;
+	}
+	else
+	{
+		*min = *n1;
+		*imin = *c1;
+		*max = *n2;
+		*imax = *c2;
+	}
+	return (1);
+}
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i;
-	int j;
+	int l1 = 0, l2 = 0, sum, exc = 0, i = size_r - 1, ret;
+	char *c1 = n1, *c2 = n2, *min, *max, *imin, *imax;
 
-	for (i = 0; n1[i] != '\0'; i++)
-		;
-	for (j = 0; n2[j] != '\0'; j++)
-		;
-
-	i -= 1;
-	j -= 1;
-
-	int c = 48;
-
-	if (i >= j)
+	ret = init(&n1, &n2, &l1, &l2, size_r, &min, &imin, &max, &imax, &c1, &c2);
+	if (!ret)
+		return (0);
+	r[i] = '\0';
+	while (min >= imin)
 	{
-		if ((i + 2) >= size_r)
-			return (0);
-
-		if (size_r > i + 1)
-			size_r -= (size_r - i - 3);
-
-		r[size_r - 1] = '\0';
-
-		for (size_r -= 2; size_r >= 0; --size_r)
-		{
-			if (j < 0)
-			{
-				if ((n1[i] + c) % 48 <= 9)
-				{
-					if (i < 0)
-						r[size_r] = c;
-					else
-						r[size_r] = (n1[i] + c) % 48 + 48;
-					c = 48;
-				}
-				else
-				{
-					r[size_r] = (n1[i] + c) % 48 + 38;
-					c = 49;
-				}
-			}
-			else
-			{
-				if ((n1[i] + n2[j] + c) % 48 <= 9)
-				{
-					r[size_r] = (n1[i] + n2[j] + c) % 48 + 48;
-					c = 48;
-				}
-				else
-				{
-					r[size_r] = (n1[i] + n2[j] + c) % 48 + 38;
-					c = 49;
-				}
-			}
-			--j;
-			--i;
-		}
+		i--;
+		sum = (*n1-- - '0') + (*n2-- - '0') + exc;
+		exc = sum / 10;
+		sum %= 10;
+		r[i] = sum + '0';
+		max--;
+		min--;
 	}
-	return (r);
+	while (max >= imax)
+	{
+		i--;
+		sum = (*max - '0') + exc;
+		exc = sum / 10;
+		sum %= 10;
+		r[i] = sum + '0';
+		max--;
+	}
+	if ((l1 == size_r - 1 || l2 == size_r - 1) && exc > 0)
+		return (0);
+	if (exc > 0)
+		r[--i] = exc + '0';
+	return (r + i);
 }
