3f1787df7b52b49f524ac7f47d5f9e6c5a25c25e
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-08-26 16:30:02 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,78 +5,33 @@
 * r: Buffer to store the result.
 * @size_r: Size of buffer r.
 * Return: Result of sum. On error return 0.
-*
-* init - Initializes pointers needed for main to work.
-* @n1: n1.
-* @n2: n2.
-* @l1: l1.
-* @l2: l2.
-* @size_r: size_r.
-* @min: min.
-* @imin: imin.
-* @max: max.
-* @imax: imax.
-* @c1: c1.
-* @c2: c2.
 */
-int init(char **n1, char **n2, int *l1, int *l2, int size_r, char **min,
-char **imin, char **max, char **imax, char **c1, char **c2)
-{
-	while (*(*n1)++)
-		(*l1)++;
-	while (*(*n2)++)
-		(*l2)++;
-	*n1 -= 2;
-	*n2 -= 2;
-	if (*l1 >= size_r || *l2 >= size_r)
-		return (0);
-	if (*l1 > *l2)
-	{
-		*min = *n2;
-		*imin = *c2;
-		*max = *n1;
-		*imax = *c1;
-	}
-	else
-	{
-		*min = *n1;
-		*imin = *c1;
-		*max = *n2;
-		*imax = *c2;
-	}
-	return (1);
-}
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1 = 0, l2 = 0, sum, exc = 0, i = size_r - 1, ret;
-	char *c1 = n1, *c2 = n2, *min, *max, *imin, *imax;
-
-	ret = init(&n1, &n2, &l1, &l2, size_r, &min, &imin, &max, &imax, &c1, &c2);
-	if (!ret)
-		return (0);
-	r[i] = '\0';
-	while (min >= imin)
-	{
-		i--;
-		sum = (*n1-- - '0') + (*n2-- - '0') + exc;
-		exc = sum / 10;
-		sum %= 10;
-		r[i] = sum + '0';
-		max--;
-		min--;
-	}
-	while (max >= imax)
-	{
-		i--;
-		sum = (*max - '0') + exc;
-		exc = sum / 10;
-		sum %= 10;
-		r[i] = sum + '0';
-		max--;
-	}
-	if ((l1 == size_r - 1 || l2 == size_r - 1) && exc > 0)
-		return (0);
-	if (exc > 0)
-		r[--i] = exc + '0';
-	return (r + i);
+int len1 = 0, len2 = 0;
+int carry = 0;
+int i, j, digit1, digit2, sum;
+while (n1[len1] != '\0')
+len1++;
+while (n2[len2] != '\0')
+len2++;
+if (len1 + 2 > size_r || len2 + 2 > size_r)
+return 0;
+r[size_r - 1] = '\0';
+i = len1 - 1;
+j = len2 - 1;
+while (i >= 0 || j >= 0 || carry) {
+digit1 = i >= 0 ? n1[i] - '0' : 0;
+digit2 = j >= 0 ? n2[j] - '0' : 0;
+sum = digit1 + digit2 + carry;
+carry = sum / 10;
+sum %= 10;
+r[size_r - 2] = sum + '0';
+i--;
+j--;
+size_r--;
+}
+if (carry > 0)
+return 0;
+return r + size_r - 1;
 }
