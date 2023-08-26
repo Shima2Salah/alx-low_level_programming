9e4045b714357a84f0d082a776a0673f3df98821
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-26 09:23:12 -0700

    a function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -6,47 +6,37 @@
  */
 void print_buffer(char *b, int size)
 {
-int i, j;
-if (size <= 0) 
-{
-printf("\n");
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%08x: ", i);
+			for (j = i; j < i + 10; j++)
+			{
+				if (j < size)
+					printf("%02x", b[j]);
+				else
+					printf("  ");
+				if (j % 2)
+					printf(" ");
+			}
+			for (j = i; j < i + 10; j++)
+			{
+				if (j >= size)
+					printf(" ");
+				else if (b[j] >= ' ' && b[j] <= '~')
+					printf("%c", b[j]);
+				else
+					printf(".");
+			}
+			printf("\n");
+		}
+	}
 }
-else
-{
-for (i = 0; i < size; i += 10)
-{
-printf("%08x: ", i);
-for (j = i; j < i + 10; j++)
-{
-if (j < size)
-{
-printf("%02x", b[j]);
-}
-else
-{
-printf("  ");
-}
-if (j % 2)
-{
-printf(" ");
-}
-} 
-for (j = i; j < i + 10; j++)
-{
-if (j >= size)
-{
-printf(" ");
-}
-else if (b[j] >= ' ' && b[j] <= '~')
-{
-printf("%c", b[j]);
-}
-else
-{
-printf(".");
-}
-}
-printf("\n");
-}
-}
-}
+
