6489b34b091b9ec4a8459938d1722cfd4d777299
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-08-29 17:13:29 -0700

    a function that locates a substrin

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,10 +11,10 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack)
 	{
 		while (*needle)
-			{
-				if (*haystack++ != *needle++)
-					break;
-			}
+		{
+			if (*haystack++ != *needle++)
+				break;
+		}
 		if (!*needle)
 		{
 			return (res);
