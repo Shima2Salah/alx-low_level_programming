c0d571d63cd55adf76477aa80eba489c6dc339da
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-01 14:35:38 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -28,10 +28,10 @@ int main(int argc, char *argv[])
 					printf("Error\n");
 					return (1);
 				}
-				sum += atoi(argv[i]);
 			}
-			printf("%d\n", sum);
+			sum += atoi(argv[i]);
 		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
