371787dbd5644283356094f24fc263c7a8b45781
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-09-01 15:51:03 +0300

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates
+ * @dest: first pointer
+ * @src: second pointer
+ *
+ * Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	char *temp;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	temp = src;
+	dest[i] = temp[0];
+	for (j = 0; temp[j] != '\0'; j++)
+	{
+		dest[i++] = temp[j];
+	}
+	temp = dest;
+	return (temp);
+}
