c5bd7b33fa38d4de19f12db69500c5589a90740d
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-04 15:37:36 -0700

    a function that creates an array of chars

---
+++ 0-create_array.c/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - fundtn creates a new array
+ * @size: array size input
+ * @c: input character
+ *
+ * Return: pointer of char
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
+
---
+++ 0-create_array.c/README.md
@@ -0,0 +1,2 @@
+My project of 0x0B. C - malloc, free
+
