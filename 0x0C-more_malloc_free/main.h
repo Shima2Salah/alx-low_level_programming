a1d0f594e20b83e2da467f1aa0f2f636a1121db5
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-09-08 02:52:36 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,8 +6,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void print_error(void);
-void check_valid(char *s);
-long int convert_to_long(char *s);
-void print_result(long int n);
 #endif
