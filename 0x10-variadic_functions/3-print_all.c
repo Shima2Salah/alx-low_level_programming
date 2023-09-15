5c0237a02a90573c4a35e3895f032e61f773c76d
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-15 05:53:29 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,44 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
 /**
 * print_all - Prints anything.
 * @format: String with format of parameters.
 */
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	short i = 0;
-	char *str;
+	va_list ptr;
+	int j = 0;
 
-	while (format)
+	va_start(ptr, format);
+	while (*(format + j) != '\0')
 	{
-		va_start(list, format);
-		while (*(format + i))
+		switch (*(format + j))
 		{
-			switch (*(format + i))
-			{
-				case 'c':
-					printf("%c", (char) va_arg(list, int));
+			case'c':
+				printf("%c", va_arg(ptr, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ptr, int));
+				break;
+			case'f':
+				printf("%f", va_arg(ptr, double));
+				break;
+			case's':
+				{
+					if (ptr == NULL)
+						printf("(nil)");
+					else
+						printf("%s", va_arg(ptr, char *));
 					break;
-				case 'i':
-					printf("%d", va_arg(list, int));
-					break;
-				case 'f':
-					printf("%f", (float) va_arg(list, double));
-					break;
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s", str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			if (*(format + i + 1))
-				printf(", ");
-			i++;
+				}
 		}
-		va_end(list);
-		break;
+		j++;
+		if (*(format + j) != '\0' && (*(format + j) == 'c' || *(format + j) == 'i'
+					|| *(format + j) == 'f' || *(format + j) == 's'))
+			printf(", ");
 	}
-		printf("\n");
+	printf("\n");
+	va_end(ptr);
 }
+
