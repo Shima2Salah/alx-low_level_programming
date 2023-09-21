05d0b6190fec1ccb0e939098a18f21b20df97f72
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-21 14:12:04 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - functn print list length
+ * @head: input derefrencing pointer
+ * @str: input pointer string
+ * Return: list_t linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+struct list_s *New_List;
+int i;
+for (i = 0; str[i] != '\0'; i++)
+;
+if (head == NULL)
+return (NULL);
+New_List = malloc(sizeof(list_t));
+if (New_List == NULL)
+return (NULL);
+New_List->str = strdup(str);
+if (New_List->str == NULL)
+{
+free(New_List);
+return (NULL);
+}
+New_List->len = i;
+New_List->next = *head;
+*head = New_List;
+}
