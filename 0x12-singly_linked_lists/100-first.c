092997180f3802341ff8dc9ae221a82fa31d6e1e
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-23 10:41:00 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-void Print_it(void) __attribute__ ((constructor));
+void Print_functn(void) __attribute__ ((constructor));
 /**
-* Print_it - Prnits a text before main.
+* Print_functn - Prnits a text before main.
 */
-void Print_it(void)
+void Print_functn(void)
 {
-	char *s = "You're beat! and yet, you must allow,\nI bore my house upon my ";
-	char *s2 = "back!\n";
-
-	printf("%s%s", s, s2);
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
