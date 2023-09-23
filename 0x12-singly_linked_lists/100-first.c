7d339d726f6357f3da45793bfe4ba96c21e61322
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-23 10:42:42 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,6 @@ void Print_functn(void) __attribute__ ((constructor));
 */
 void Print_functn(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must
+			allow,\nI bore my house upon my back!\n");
 }
