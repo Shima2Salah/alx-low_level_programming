d776ae601ad0ba8920b0204335b70ed2fcd289db
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-25 14:49:43 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - functn to get index node
+ * @head: input struct pointer
+ * @index: input integer
+ * Return: list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *p;
+
+	if (!head)
+		return (0);
+	p = head;
+	while (i < index)
+	{
+		p = p->next;
+		i++;
+	}
+	return (p);
+}
