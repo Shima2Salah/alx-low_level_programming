5f1a5000b79aaf30fdd8773ae7013c1d4c67b0df
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-25 13:25:44 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - functn to free node
+ * @head: input struct pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head != NULL)
+	{
+		p = *head;
+		*head = p->next;
+		free(p);
+	}
+}
