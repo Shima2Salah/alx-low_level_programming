088128f728b33d5589dd611e7350943cae8957f1
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-26 12:13:01 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,36 +10,37 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *New_list;
-listint_t *p;
-unsigned int i = 0;
-if ((head == NULL) || (!*head && idx))
-return (NULL);
-p = *head;
-if (*head == NULL || idx == 0)
-{
-New_list = malloc(sizeof(listint_t));
-if (New_list == NULL)
-return (NULL);
-New_list->n = n;
-New_list->next = *head;
-*head = New_list;
-}
-else
-{
-while (i < idx - 1)
-{
-if (p->next == NULL)
-return (NULL);
-p = p->next;
-i++;
-}
-New_list = malloc(sizeof(listint_t));
-if (New_list == NULL)
-return (NULL);
-New_list->n = n;
-New_list->next = p->next;
-p->next = New_list;
-}
-return (New_list);
+	listint_t *New_list;
+	listint_t *p;
+	unsigned int i = 0;
+
+	if ((head == NULL) || (!*head && idx))
+		return (NULL);
+	p = *head;
+	if (*head == NULL || idx == 0)
+	{
+		New_list = malloc(sizeof(listint_t));
+		if (New_list == NULL)
+			return (NULL);
+		New_list->n = n;
+		New_list->next = *head;
+		*head = New_list;
+	}
+	else
+	{
+		while (i < idx - 1)
+		{
+			if (p->next == NULL)
+				return (NULL);
+			p = p->next;
+			i++;
+		}
+		New_list = malloc(sizeof(listint_t));
+		if (New_list == NULL)
+			return (NULL);
+		New_list->n = n;
+		New_list->next = p->next;
+		p->next = New_list;
+	}
+	return (New_list);
 }
