ef2172d1a271a1f4fc2ee9229fd97f4d62edc3ac
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-28 12:44:41 -0700

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,10 +5,7 @@
 */
 void print_binary(unsigned long int n)
 {
-int c = sizeof(n) * 8;
-while (c > 0)
-{
-c--;
-_putchar(((n >> c) &1) + '0');
-}
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
