2d519c3a651d40729611be4bfc330c532860c13e
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-28 13:11:14 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit - functn to print binary
+ * @n: input unsigned long in
+ * @index: input unsigned int
+ * Return: integer number
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+int a;
+if (index < 0)
+return (-1);
+a = (n >> (index)) & 1;
+return (a);
+}
