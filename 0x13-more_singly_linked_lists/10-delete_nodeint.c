1a30665122955bff4000476e66fcdfa7e9b782b9
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-09-29 02:36:30 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,35 +1,37 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 /**
-* delete_nodeint_at_index - Deletes node at index of a listint_t linked list.
-* @head: Pointer to pointer to the linked list.
-* @index: Index of the node to be deleted.
-* Return: 1 if succeeded, -1 if not.
-*/
+ * delete_nodeint_at_index - functn to add node
+ * @head: input struct pointer
+ * @index: input index
+ * Return: integer
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *iterator = *head, *temp;
-
-	if (!*head)
-		return (-1);
-
-	if (!index)
-	{
-		*head = iterator->next;
-		free(iterator);
-		return (1);
-	}
-	while (index > 1)
-	{
-		iterator = iterator->next;
-		if (!iterator)
-			return (-1);
-		index--;
-	}
-	temp = iterator->next;
-	if (!temp)
-		return (-1);
-	iterator->next = temp->next;
-	free(temp);
-	return (1);
+listint_t *p;
+listint_t *temp;
+unsigned int i = 1;
+if ((head == NULL) || (!*head && index))
+return (-1);
+p = *head;
+if (index == 0)
+{
+*head = p->next;
+free(p);
+return (1);
 }
+while ((i < index - 1) && (p != NULL))
+{
+if (p->next == NULL)
+return (-1);
+p = p->next;
+i++;
+}
+temp = p->next;
+if (!temp)
+return (-1);
+p->next = temp->next;
+free(temp);
+return (1);
+}
+
