aa431c34cef0f838d5e84ddc4c34492512427877
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-10-03 00:44:50 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,41 @@
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
- * read_textfile - functn to read filename
- * @filename: input character
- * @letters: input integer
- * Return: integer size
- */
+* read_textfile - Reads a text file and prints it to the POSIX standard output.
+* @filename: Name of the file to be read.
+* @letters: Number of letters it should read and print.
+* Return: Number of letters it could read and print.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *fptr;
-size_t i = 0;
-char ch;
-fptr = fopen(filename, "r");
-if ((fptr == NULL) || (filename == NULL))
-return (0);
-while (((ch = getc(fptr)) != EOF) && i < letters)
-{
-_putchar(ch);
-if (ch != '\n')
-i++;
-}
-fclose(fptr);
-return (i);
+	int file_descriptor, nchars, write_check;
+	char *buffer;
+
+	if (!filename || !letters)
+		return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor < 0)
+		return (0);
+	buffer = malloc(letters + 1);
+	if (!buffer)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+	nchars = read(file_descriptor, buffer, letters);
+	if (nchars <= 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+	write_check = write(STDOUT_FILENO, buffer, nchars);
+	if (write_check < nchars)
+		return (0);
+	free(buffer);
+	close(file_descriptor);
+	return (nchars);
 }
