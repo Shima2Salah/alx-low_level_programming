38af283c21ab6e944001d4071fb0a8336202ad3f
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-09 14:45:25 -0800

    more_double_linked_lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,21 +9,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *current = *head;
 unsigned int i = 0;
-if (head == NULL)
+if (head == NULL || *head == NULL)
 return (-1);
 if (index == 0)
 {
 *head = current->next;
+if (*head != NULL)
+(*head)->prev = NULL;
 free(current);
 return (1);
 }
-while (i < index)
+while (current != NULL && i < index)
 {
-if (current == NULL)
-return (-1);
 current = current->next;
 i++;
 }
+if (current == NULL)
+return (-1);
 if (current->next == NULL)
 {
 current->prev->next = NULL;
