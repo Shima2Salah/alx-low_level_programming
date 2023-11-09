7bf0977c42e3bd7ccb3b2ff164f89aa3060cff9a
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-11-09 09:18:53 -0800

    more_double_linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,7 +7,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new;
+dlistint_t* new;
 if (head == NULL)
 return (NULL);
 new = malloc(sizeof(dlistint_t));
@@ -16,6 +16,8 @@ return (NULL);
 new->n = n;
 new->prev = NULL;
 new->next = *head;
+if (*head != NULL)
+(*head)->prev = new;
 *head = new;
 return (new);
 }
