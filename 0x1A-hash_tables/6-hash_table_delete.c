049e04d550bce7a1b18416712f78a9cabc1cb13b
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2023-12-01 23:58:25 +0200

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,27 +1,34 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_delete - func delete hash table
- * @ht: hash table
+ * hash_table_delete - delete hash table
+ * @ht: the hashtable
+ * Return: null
  */
 void hash_table_delete(hash_table_t *ht)
 {
-unsigned long int idx;
-hash_node_t *current;
-hash_node_t *helper;
-if (!ht)
-return;
-for (idx = 0; idx < ht->size; idx++)
-{
-current = ht->array[idx];
-while (current)
-{
-helper = current->next;
-free(current->key);
-free(current->value);
-free(current);
-current = helper;
-}
-}
-free(ht->array);
-free(ht);
+	unsigned long int i;
+	hash_table_t *table = ht;
+	hash_node_t *new, *tmp;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			new = ht->array[i];
+			while (new != NULL)
+			{
+				tmp = new->next;
+				free(new->key);
+				free(new->value);
+				free(new);
+				new = tmp;
+			}
+		}
+	}
+	free(table->array);
+	free(table);
+
 }
