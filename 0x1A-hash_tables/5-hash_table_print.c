e97e611e80ff4b7f1d3777e9d81f87620ab3bc9d
Shimaa Salah <shimaasalah91.13@gmail.com>
2023-12-01 13:22:24 -0800

    Hash tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,10 +5,20 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-unsigned long int idx;
+unsigned long int idx, i = 0, count = 0;
 hash_node_t *current;
 if (!ht)
 return;
+for (idx = 0; idx < ht->size; idx++)
+{
+current = ht->array[idx];
+while (current)
+{
+if (current->key != NULL)
+count ++;
+current = current->next;
+}
+}
 printf("{");
 for (idx = 0; idx < ht->size; idx++)
 {
@@ -16,8 +26,11 @@ current = ht->array[idx];
 while (current)
 {
 printf("'%s': '%s'", current->key, current->value);
-if (current->next != NULL)
+if (i < count -1)
+{
 printf(", ");
+i++;
+}
 current = current->next;
 }
 }
