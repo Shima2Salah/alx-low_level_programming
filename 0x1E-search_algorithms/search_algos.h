59eecbc214f4a27ffc5b8e252dc1aaee43bf47a2
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-03-12 12:53:14 -0700

    search_algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef search_algos_h
+#define search_algos_h
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+#endif
