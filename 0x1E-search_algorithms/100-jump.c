0a6ea6919ca6ceaf62cf82e82114f8a23f6ab468
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-13 15:18:06 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,25 +10,25 @@
 */
 int jump_search(int *array, size_t size, int value)
 {
-	unsigned int sqroot, i, j;
+unsigned int sqroot, i, j;
 
-	if (!array || !size || array[0] > value)
-		return (-1);
-	sqroot = sqrt(size);
-	for (i = 0; i < size; i += sqroot)
-	{
-		if (array[i] >= value)
-			break;
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-	}
-	printf("Value found between indexes [%d] and [%d]\n", i - sqroot, i);
-	for (j = i - sqroot; j <= i; j++)
-	{
-		if (j >= size)
-			return (-1);
-		printf("Value checked array[%d] = [%d]\n", j, array[j]);
-		if (array[j] == value)
-			return (j);
-	}
-	return (-1);
+if (!array || !size || array[0] > value)
+return (-1);
+sqroot = sqrt(size);
+for (i = 0; i < size; i += sqroot)
+{
+if (array[i] >= value)
+break;
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
+}
+printf("Value found between indexes [%d] and [%d]\n", i - sqroot, i);
+for (j = i - sqroot; j <= i; j++)
+{
+if (j >= size)
+return (-1);
+printf("Value checked array[%d] = [%d]\n", j, array[j]);
+if (array[j] == value)
+return (j);
+}
+return (-1);
 }
