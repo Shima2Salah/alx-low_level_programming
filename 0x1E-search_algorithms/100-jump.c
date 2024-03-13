d229473ae8bacd242fc22b5f6d969ec338e19882
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-03-13 06:15:48 -0700

    search_algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,18 +14,16 @@ int jump_search(int *array, size_t size, int value)
 {
 size_t m, i = 0, j;
 m = sqrt(size);
-if (array == NULL  || !size || array[0] > value)
+if (array == NULL)
 return (-1);
 while (i < size)
 {
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 i = i + m;
-if (array[i] >= value || i >= size)
-{
-printf("Value found between indexes [%ld] and [%ld]\n", i - m, i);
+if (array[i] >= value)
 break;
 }
-}
+printf("Value found between indexes [%ld] and [%ld]\n", i - m, i);
 for (j = i - m; j <= i && j < size; j++)
 {
 printf("Value checked array[%ld] = [%d]\n", j, array[j]);
