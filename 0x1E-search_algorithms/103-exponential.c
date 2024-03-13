b2b07e5ec53ce091821e9594879b31f9858d90ba
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-03-13 13:55:40 -0700

    search_algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -13,7 +13,7 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-size_t i = 1, j;
+size_t i = 1, j, max;
 if (array == NULL)
 return (-1);
 while (i < size && array[i] < value)
@@ -21,8 +21,12 @@ while (i < size && array[i] < value)
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 i = i * 2;
 }
-printf("Value found between indexes [%ld] and [%ld]\n", i / 2, i - 1);
-j = _binary_search(array, i / 2, i - 1, value);
+if (i > size - 1)
+max = size - 1;
+else
+max = i;
+printf("Value found between indexes [%ld] and [%ld]\n", i / 2, max);
+j = _binary_search(array, i / 2, max, value);
 return (j);
 }
 
