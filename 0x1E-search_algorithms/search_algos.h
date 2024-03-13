585493a8916790d5bf9892e38beec4de9ac091ba
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-03-13 23:24:43 +0200

    search_algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,6 +44,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int recursive_search(int *array, int left, int right, int value);
 int _binary_search(int *array, size_t l, size_t h, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
